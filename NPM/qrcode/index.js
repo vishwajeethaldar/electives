dd1dabfe57bb56be828a93c76864b4afabf17e12
vishwajeethaldar <onlyjeet3@gmail.com>
2022-10-05 17:48:16 +0530

    qr coe

--- NPM/qrcode/index.js
+++ NPM/qrcode/index.js
@@ -1,5 +1,12 @@
 var QRCode = require('qrcode')
+const { program } = require('commander');
 
+program.arguments("<qrcodetext>","qr code text")
+    .action(function(qrcodetext){
+        QRCode.toString(qrcodetext,{type:'terminal'}, function (err, url) {
+            console.log(url)
+          })
+    })
 // QRCode.toDataURL('I am a pony!', function (err, url) {
 //   console.log(url)
 // })
